a640529a43a482d56333d6a0897d513581506316
harborsiem <harborsiem@gmail.com>
2016-09-21 08:07:07 +0000

    WIX Version 3.10

--- trunc/Sudoku/Build.bat
+++ trunc/Sudoku/Build.bat
@@ -7,11 +7,11 @@ rem Set MsBuildPath="%ProgramFiles%\MsBuild\14.0\bin\amd64\MsBuild.exe"
 rem 32Bit Systems
 rem Set MsBuildPath="%windir%\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe"
 Set MsBuildPath="%ProgramFiles%\MsBuild\14.0\bin\MsBuild.exe"
-Set Wix35Path="%ProgramFiles%\Windows Installer XML v3.5\bin
+Set Wix3xPath=%ProgramFiles%\WiX Toolset v3.10\bin
 rem cd %AppName%
 %MsBuildPath% %AppName%.sln /p:Configuration=Debug /p:Platform="Any CPU"
 %MsBuildPath% %AppName%.sln /p:Configuration=Release /p:Platform="Any CPU"
 rem cd ..
-%Wix35Path%\candle.exe" -nologo %AppName%.wxs -out %AppName%.wixobj  -ext WixUIExtension  -ext WixNetFxExtension
-%Wix35Path%\light.exe" -nologo %AppName%.wixobj -out %AppName%.msi  -ext WixUIExtension  -ext WixNetFxExtension
+"%Wix3xPath%\candle.exe" -nologo %AppName%.wxs -out %AppName%.wixobj  -ext WixUIExtension  -ext WixNetFxExtension
+"%Wix3xPath%\light.exe" -nologo %AppName%.wixobj -out %AppName%.msi  -ext WixUIExtension  -ext WixNetFxExtension
 pause
