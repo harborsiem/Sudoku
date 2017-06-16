f6370448a11d22a015897edf18ef25828b113345
harborsiem <harborsiem@gmail.com>
2017-06-16 09:38:00 +0000

    Wix 3.11

--- trunc/Sudoku/Build.bat
+++ trunc/Sudoku/Build.bat
@@ -7,7 +7,7 @@ rem Set MsBuildPath="%ProgramFiles%\MsBuild\14.0\bin\amd64\MsBuild.exe"
 rem 32Bit Systems
 rem Set MsBuildPath="%windir%\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe"
 Set MsBuildPath="%ProgramFiles%\MsBuild\14.0\bin\MsBuild.exe"
-Set Wix3xPath=%ProgramFiles%\WiX Toolset v3.10\bin
+Set Wix3xPath=%ProgramFiles%\WiX Toolset v3.11\bin
 rem cd %AppName%
 %MsBuildPath% %AppName%.sln /p:Configuration=Debug /p:Platform="Any CPU"
 %MsBuildPath% %AppName%.sln /p:Configuration=Release /p:Platform="Any CPU"
