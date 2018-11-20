e7ddcbfa9e93661ea4bf29b627ca9567e89f0a06
harborsiem <harbor@herzovision.de>
2018-11-20 10:46:13 +0100

    Build changes for Windows 64 Bit (Win10)

--- trunc/Sudoku/Build.bat
+++ trunc/Sudoku/Build.bat
@@ -1,13 +1,22 @@
 Set AppName="Sudoku"
 
+if not defined ProgramFiles(x86) goto x86
 rem 64Bit Systems
-rem Set MsBuildPath="%windir%\Microsoft.NET\Framework64\v4.0.30319\MsBuild.exe"
-rem Set MsBuildPath="%ProgramFiles%\MsBuild\14.0\bin\amd64\MsBuild.exe"
-
-rem 32Bit Systems
+  Set X86ProgramFiles=%ProgramFiles(x86)%
 rem Set MsBuildPath="%windir%\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe"
-Set MsBuildPath="%ProgramFiles%\MsBuild\14.0\bin\MsBuild.exe"
-Set Wix3xPath=%ProgramFiles%\WiX Toolset v3.11\bin
+Set MsBuildPath="%X86ProgramFiles%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
+rem only 64 bit projects  Set MsBuildPath="%X86ProgramFiles%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MsBuild.exe"
+rem Set MsBuildPath="%windir%\Microsoft.NET\Framework64\v4.0.30319\MsBuild.exe"
+  goto x86End
+
+:x86
+rem 32Bit Systems
+  Set X86ProgramFiles=%ProgramFiles%
+rem Set MsBuildPath="%windir%\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe"
+Set MsBuildPath="%ProgramFiles%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
+:x86End
+
+Set Wix3xPath=%X86ProgramFiles%\WiX Toolset v3.11\bin
 rem cd %AppName%
 %MsBuildPath% %AppName%.sln /p:Configuration=Debug /p:Platform="Any CPU"
 %MsBuildPath% %AppName%.sln /p:Configuration=Release /p:Platform="Any CPU"
